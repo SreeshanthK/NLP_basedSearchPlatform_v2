6ee934467dbd33ff32be5cea10090d092fc2d8b8
Ankur Akash <07ankur007@gmail.com>
2025-07-21 12:17:25 +0530

    Update authService.js

--- react-frontend/src/services/authService.js
+++ react-frontend/src/services/authService.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_BASE_URL = 'http://localhost:8001/api/';
+const API_BASE_URL = 'https://search-relevance-optimizer-backend.onrender.com/';
 
 const api = axios.create({
     baseURL: API_BASE_URL,
