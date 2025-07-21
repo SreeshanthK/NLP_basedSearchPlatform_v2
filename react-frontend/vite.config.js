da5568e07e0e34e34deb44d8f2f374f9ec3eb8e9
Ankur Akash <07ankur007@gmail.com>
2025-07-21 13:06:13 +0530

    Update vite.config.js

--- react-frontend/vite.config.js
+++ react-frontend/vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  server: {\
+  server: {
     proxy: {
       '/api': {
         target: 'https://search-relevance-optimizer-backend.onrender.com',
